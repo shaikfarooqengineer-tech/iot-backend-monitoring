9ecdd0aeb538d98c3971b71c0c513228f958779c
shaikfarooqengineer-tech <shaikfarooqengineer@gmail.com>
2026-07-23 11:23:16 +0530

    Merge fix (#5)
    * feat: implement async MQTT consumer, ingestion queue, and telemetry routing service with data validation and persistence modules
    
    * feature: implement MQTT telemetry ingestion, validation, and storage service with Pydantic models
    
    * feat: implement telemetry ingestion pipeline with
    MQTT consumer, Pydantic models, Valkey caching, and MongoDB persistence.
    
    * merge fix
    
    * feat: implement MQTT consumer service and background data processing pipeline with Valkey and MongoDB support
    
    * added pydantic-settings
    
    * feat: initialize application configuration module
    
    ---------
    
    Co-authored-by: shreyasraj-001 <shreyasraj735@gmail.com>

--- app/config.py
+++ app/config.py
@@ -2,7 +2,6 @@
 # FILE: backend/app/config.py
 #  ══════════════════════════════════════════════════════════════════════════════
 
-from pydantic import BaseModel
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
 import os
@@ -11,12 +10,12 @@ load_dotenv()
 
 class Settings(BaseSettings):
     # MQTT Configuration
-    mqtt_host: str = os.getenv("MQTT_HOST", "localhost")
-    mqtt_port: int = int(os.getenv("MQTT_PORT", 1883))
-    mqtt_topic: str = os.getenv("MQTT_TOPIC", "hospital/telemetry/#")
-    mqtt_username: str | None = os.getenv("MQTT_USERNAME", None)
-    mqtt_password: str | None = os.getenv("MQTT_PASSWORD", None)
-    mqtt_use_tls: bool = os.getenv("MQTT_USE_TLS", "false").lower() == "true"
+    mqtt_host: str = os.getenv("MQTT_HOST", "a2f21c1405bc437b9508ec02e6bd8aa0.s1.eu.hivemq.cloud")
+    mqtt_port: int = int(os.getenv("MQTT_PORT", 8883))
+    mqtt_topic: str = os.getenv("MQTT_TOPIC", "test/olt/esp32/#")
+    mqtt_username: str | None = os.getenv("MQTT_USERNAME", "olt_sleep")
+    mqtt_password: str | None = os.getenv("MQTT_PASSWORD", "Olt@1234")
+    mqtt_use_tls: bool = os.getenv("MQTT_USE_TLS", "true").lower() == "true"
     
     # Local Queue Configuration
     queue_max_size: int = int(os.getenv("QUEUE_MAX_SIZE", 10000))
@@ -26,11 +25,11 @@ class Settings(BaseSettings):
     
     # MongoDB Configuration
     mongo_uri: str = os.getenv("MONGO_URI", "")
-    mongo_db_name: str = os.getenv("MONGO_DB_NAME")
+    mongo_db_name: str = os.getenv("MONGO_DB_NAME", "vitals_monitoring")
     # Valkey / Redis Configuration
     valkey_host: str = os.getenv("VALKEY_HOST", "localhost")
-    valkey_port: int = os.getenv("VALKEY_PORT", 6379)
-    valkey_channel: str = os.getenv("VALKEY_CHANNEL", "telemetry_events")
-    valkey_password: str | None = os.getenv("VALKEY_PASSWORD", None)
+    valkey_port: int = int(os.getenv("VALKEY_PORT", 6379))
+    valkey_channel: str = os.getenv("VALKEY_CHANNEL", "hospital.telemetry.events")
+    valkey_password: str | None = os.getenv("VALKEY_PASSWORD", "25fac5c6f53ce29324bf08855d335a61c204754436a4511843785987d6bc6279")
 
 settings = Settings()
